7627471f16adfbc7abd562fb8fc692cd47dc7e58
vitorclaudio <vitorcldeosantos@gmail.com>
2023-10-16 21:08:40 -0300

    Desenvolvimento da tela home, primeiro e segundo card.

--- commercefrontend/src/Pages/Home/Home.js
+++ commercefrontend/src/Pages/Home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Home.css';
+import logoImageUrl from '../../Images/Logo/logoProvisoria.png';
+import handImage from '../../Images/Home/hand-holding-smartphone-with-screen-mockup-png.webp'; // adjust the path according to your project structure
+import PlansImage from '../../Images/Home/0bf7feb3b117c6b6924558ddaf9957cf-removebg-preview.png'; // adjust the path according to your project structure
+import BuildStoreImage from '../../Images/Home/undraw_web_shopping_re_owap.png'; // adjust the path according to your project structure
+import ExploreAndSelectImage from '../../Images/Home/undraw_Window_shopping_re_0kbm-removebg-preview.png'; // adjust the path according to your project structure
+import RedirectToWppImage from '../../Images/Home/whatsapp-no-ecommerce.jpg'; // adjust the path according to your project structure
 
 function Home() {
+
+
     return (
         <div className="app-container">
             <header className="header">
                 <div style={{marginLeft: '10px', marginTop: '10px'}}>
-                    <div>MY</div>
-                    <div>APP</div>
+                    <img src={logoImageUrl} alt="Logo" className="small-image" />
                 </div>
                 <nav>
                     <div className="headerText" href="#">Planos</div>
@@ -23,21 +30,75 @@ function Home() {
                 </nav>
             </header>
 
-            <div>
 
+
+            <section className="first-card">
+
+                <div className="firstCard-text-section">
+                    <h2 style={{color: '#129666'}}>Crie seu catálogo virtual</h2>
+                    <p style={{color: '#889F7C'}}>e conecte-se diretamente com seus clientes pelo WhatsApp!</p>
+                    <button>Comece Agora</button>
+                </div>
+
+                <div className="image-container">
+                    <img src={handImage}  className="contained-image" />
+                </div>
+            </section>
+
+            <div className="second-card">
+                <div className="secondCard-text-title" >Fácil e rápido</div>
+                <div className="secondCard-text-subtitle" >Comece agora seguindo os seguintes passos</div>
+
+                <div style={{backgroundColor: '#DCD9F8'}}className="secondCard-image-container">
+                    <img src={PlansImage} className="secondCard-contained-image" />
+                   <div className="secondCard-text-section">
+                       <div className='secondCard-text-section-title' style={{color: '#333333'}}>Escolha o seu plano</div>
+                       <div className='secondCard-text-section-description'>Barato e acessível</div>
+
+                   </div>
+
+                </div>
+
+                <div style={{backgroundColor: '#FFE6DF'}}className="secondCard-image-container">
+                    <img style={{borderRadius:'30px', height:'80%', marginRight:'20px'}}  src={BuildStoreImage} className="secondCard-contained-image" />
+                    <div className="secondCard-text-section">
+                        <div className='secondCard-text-section-title' style={{color: '#333333'}}>Crie sua loja virtual</div>
+                        <div className='secondCard-text-section-description'>Adicione seus produtos
+                            ou serviços </div>
+
+                    </div>
+
+                </div>
+
+                <div style={{backgroundColor: '#FCE2EB'}}className="secondCard-image-container">
+                    <img src={ExploreAndSelectImage}  className="secondCard-contained-image" />
+                    <div className="secondCard-text-section">
+                        <div className='secondCard-text-section-title' style={{color: '#333333'}}>Explore e
+                            Selecione</div>
+                        <div className='secondCard-text-section-description'>O cliente será direcionado
+                            para o seu whatsapp com
+                            a lista de pedidos
+                            </div>
+
+                    </div>
+
+                </div>
+
+                <div style={{backgroundColor: '#DCF4F4'}}className="secondCard-image-container">
+                    <img style={{borderRadius:'10px', height: '100%', width: '250px', marginLeft:'0px'}} src={RedirectToWppImage} className="secondCard-contained-image" />
+                    <div className="secondCard-text-section">
+                        <div className='secondCard-text-section-title' style={{color: '#333333'}}>Feche a venda
+                            pelo whatsapp</div>
+                        <div className='secondCard-text-section-description'>O cliente será direcionado
+                            para o seu whatsapp com
+                            a lista de pedidos</div>
+
+                    </div>
+
+                </div>
 
             </div>
 
-            <section className="catalog-section">
-                <div className="firstCard-text-section">
-                    <h2>Crie seu catálogo virtual</h2>
-                    <p>e conecte-se diretamente com seus clientes pelo WhatsApp!</p>
-                    <button>Comece Agora</button>
-                </div>
-                <div>
-
-                </div>
-            </section>
 
             <footer>
                 <p>MY APP</p>
