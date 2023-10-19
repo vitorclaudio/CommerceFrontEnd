0bea9b14b632b74efefc2506d16a65b1458b6fcd
vitorclaudio <vitorcldeosantos@gmail.com>
2023-10-18 23:19:06 -0300

    Updates

---
+++ precache-manifest.3bc91d7eeb8e967d0b978f483b9aa47b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edb68af376d5d75e4180c9f2a6e8d92",
+    "url": "/CommerceFrontEnd/index.html"
+  },
+  {
+    "revision": "23d118fc4b457253ff73",
+    "url": "/CommerceFrontEnd/static/css/main.f4eb3d84.chunk.css"
+  },
+  {
+    "revision": "2a7885d4964a5bf58260",
+    "url": "/CommerceFrontEnd/static/js/2.05c505a4.chunk.js"
+  },
+  {
+    "revision": "6885d93491a5f1f9f154",
+    "url": "/CommerceFrontEnd/static/js/3.4f83f070.chunk.js"
+  },
+  {
+    "revision": "23d118fc4b457253ff73",
+    "url": "/CommerceFrontEnd/static/js/main.30af16e7.chunk.js"
+  },
+  {
+    "revision": "b6fb4f9169e06859cf30",
+    "url": "/CommerceFrontEnd/static/js/runtime~main.600c19d8.js"
+  },
+  {
+    "revision": "029760cde9cd67a56d3d09158778d203",
+    "url": "/CommerceFrontEnd/static/media/handphone.029760cd.webp"
+  },
+  {
+    "revision": "c9aa09d1e72658b4cf95d606bfbc9ed8",
+    "url": "/CommerceFrontEnd/static/media/logoProvisoria.c9aa09d1.png"
+  },
+  {
+    "revision": "0215dc75a630b20563794aeb8d7eb632",
+    "url": "/CommerceFrontEnd/static/media/pickshop.0215dc75.png"
+  },
+  {
+    "revision": "0c3b75f0e196efe177e1808b23c1f7d3",
+    "url": "/CommerceFrontEnd/static/media/planosexemplo.0c3b75f0.png"
+  },
+  {
+    "revision": "332e95bde3496795adb11b2ad1a9ed0c",
+    "url": "/CommerceFrontEnd/static/media/shop.332e95bd.png"
+  },
+  {
+    "revision": "cfee4a905ab4556608edb36dd85b73c4",
+    "url": "/CommerceFrontEnd/static/media/whatsapp-no-ecommerce.cfee4a90.jpg"
+  }
+]);
