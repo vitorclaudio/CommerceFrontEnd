dada236947e273fcb3364be1b744c2d6db971c06
vitorclaudio <vitorcldeosantos@gmail.com>
2023-10-18 22:17:04 -0300

    Tela home mobile first finalizado

--- commercefrontend/src/Pages/Home/Home.js
+++ commercefrontend/src/Pages/Home/Home.js
@@ -6,6 +6,7 @@ import PlansImage from '../../Images/Home/0bf7feb3b117c6b6924558ddaf9957cf-remov
 import BuildStoreImage from '../../Images/Home/undraw_web_shopping_re_owap.png'; // adjust the path according to your project structure
 import ExploreAndSelectImage from '../../Images/Home/undraw_Window_shopping_re_0kbm-removebg-preview.png'; // adjust the path according to your project structure
 import RedirectToWppImage from '../../Images/Home/whatsapp-no-ecommerce.jpg'; // adjust the path according to your project structure
+import AllPlans from '../../Images/Home/subscription-plans-with-account-features-list-included-options-price_189959-1155__1_-removebg-preview.png'; // adjust the path according to your project structure
 
 function Home() {
 
@@ -28,6 +29,7 @@ function Home() {
                 <nav>
                     <button href="#">Entrar</button>
                 </nav>
+
             </header>
 
 
@@ -85,8 +87,8 @@ function Home() {
                 </div>
 
                 <div style={{backgroundColor: '#DCF4F4'}}className="secondCard-image-container">
-                    <img style={{borderRadius:'10px', height: '100%', width: '250px', marginLeft:'0px'}} src={RedirectToWppImage} className="secondCard-contained-image" />
-                    <div className="secondCard-text-section">
+                    <img style={{borderRadius:'10px', height: '100%', width: '65%', marginLeft:'0px'}} src={RedirectToWppImage} className="secondCard-contained-image" />
+                    <div style={{marginLeft:'5px'}}className="secondCard-text-section">
                         <div className='secondCard-text-section-title' style={{color: '#333333'}}>Feche a venda
                             pelo whatsapp</div>
                         <div className='secondCard-text-section-description'>O cliente será direcionado
@@ -100,12 +102,73 @@ function Home() {
             </div>
 
 
+            <div className='third-card-container'>
+                <div className='third-card-contained-square'>
+                    <h2 style={{color: '#129666'}}>Planos</h2>
+                </div>
+                <div className='cards-container'>
+                    <div className="card" style={{ background: 'linear-gradient(to bottom, #00A39C, #006A42)' }}>
+                        <div className="card-header">Mensal</div>
+                        <div className="card-content">
+                            <p>ASSINE MENSALMENTE</p>
+                            <p>E CRIE SEU CATÁLOGO VIRTUAL</p>
+                        </div>
+                        <div className="card-price">
+                            <span>R$<sup>14,99</sup></span>
+                            <span className="price-period">POR MÊS</span>
+                        </div>
+                        <button className="buy-button">ASSINAR</button>
+                    </div>
+
+                    <div className="card" style={{ background: 'linear-gradient(to bottom, #8461A3, #006A42)' }}>
+                        <div className="card-header">Trimestral</div>
+                        <div className="card-content">
+                            <p>ASSINE O TRIMESTRAL</p>
+                            <p>E CRIE SEU CATÁLOGO VIRTUAL</p>
+                        </div>
+                        <div className="card-price">
+                            <span>R$<sup>39,99</sup></span>
+                            <span className="price-period">POR 3 MESES</span>
+                        </div>
+                        <button className="buy-button">ASSINAR</button>
+                    </div>
+
+                    <div className="card" style={{ background: 'linear-gradient(to bottom, #FF7694, #006A42)' }}>
+                        <div className="card-header">Anual</div>
+                        <div className="card-content">
+                            <p>ASSINE ANUALMENTE</p>
+                            <p>E CRIE SEU CATÁLOGO VIRTUAL</p>
+                        </div>
+                        <div className="card-price">
+                            <span>R$<sup>149,99</sup></span>
+                            <span className="price-period">POR 1 ANO</span>
+                        </div>
+                        <button className="buy-button">ASSINAR</button>
+                    </div>
+                </div>
+
+
+            </div>
+            <div style={{backgroundColor: '#006A42', marginTop: '-1px'}}>
+                <div className='fourth-card-container'>
+                    <div style={{color: 'white', fontSize:'250%', fontWeight:'bolder', marginBottom:'10px'}}>Junte-se a nós</div>
+                    <div style={{color: 'white', fontSize:'120%', fontWeight:'bold', marginBottom:'10px'}}>Exponha seus produtos e</div>
+                    <div style={{color: 'white', fontSize:'100%'}}>Exponha seus produtos e</div>
+                </div>
+
+            </div>
+
+
+
             <footer>
-                <p>MY APP</p>
-                <a href="#">termos de serviço</a>
-                <a href="#">política de privacidade</a>
-                <a href="#">Contate-nos</a>
-                <p>MyApp 2023. All rights reserved</p>
+                <p style={{marginLeft: '10px'}}>MY APP</p>
+                <div style={{display:'flex'}}>
+                    <div style={{fontSize:'60%', marginLeft:'10px'}} href="#">termos de serviço</div>
+                    <div style={{fontSize:'60%', marginLeft:'10px'}}href="#">política de privacidade</div>
+                    <div style={{fontSize:'60%', marginLeft:'10px'}}href="#">Contate-nos</div>
+                    <div style={{fontSize:'60%', marginLeft:'10px', marginRight:'10px'}}>MyApp 2023. All rights reserved</div>
+                </div>
+
             </footer>
         </div>
     );
